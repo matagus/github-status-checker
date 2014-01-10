8259a99af962bffa47042e6a161c84ee5a682323
Agustín Méndez <matagus@gmail.com>
2014-01-10 18:28:26 -0300

    New default compact gui mode configurable via preferences.

--- data/periodical-checker.js
+++ data/periodical-checker.js
@@ -1,5 +1,10 @@
+var containerElem = document.querySelector("#container");
 var statusElem = document.querySelector("#status");
 
 self.port.on("update-status", function(status, last_updated) {
   statusElem.className = status;
+});
+
+self.port.on("change-gui-mode", function(mode) {
+  containerElem.className = mode;
 });
